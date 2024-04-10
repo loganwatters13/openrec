6510b320c1f4cf69fc69e2bd5ddda69f3c7051ca
loganwatters13 <156961568+loganwatters13@users.noreply.github.com>
2024-04-10 17:12:26 -0500

    Update ucml.py

--- openrec/recommenders/ucml.py
+++ openrec/recommenders/ucml.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from openrec.recommenders import BPR
 from openrec.modules.interactions import PairwiseEuDist
 from openrec.modules.extractions import LatentFactor
